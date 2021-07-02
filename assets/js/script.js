b6476f0c517e30c71f1b576290d999ddd1ac5978
Josh Gumperz <joshg3023@gmail.com>
2021-07-01 23:18:40 -0700

    added descriptive comments to index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// declare variables
 var timeTracker = moment().format("hA")
 var hourTracker = parseInt(moment().format("h"))
 var dayTracker = moment().format("dddd")
@@ -15,35 +16,47 @@ var threePm = $("#15");
 var fourPm = $("#16");
 var fivePm = $("#17");
 var saveEvent
-var userInput 
+var userTask
 var saveEvent
 var saveBlock
+// Declare array that contains the all same numbers as the IDs of the input fields
+var eventArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-eventArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+// display day of week and date on header
 dateDisplay.text(dayTracker + ", " + dateTracker)
 
+// when save button is clicked execute this function
 saveBtn.on("click", function (event) {
+    // target which save button was clicked
     var element = event.target;
-    console.log(element)
+    // store the input the user typed in the input box associated with the targetted save button in a variable
     userTask = $(element).siblings().children().val()
+    // get the id of the parent element of the input field for later use
     var userHour = $(element).siblings().children().attr("id")
-    console.log(userHour)
+    // save userHour and userTask to local storage
     localStorage.setItem(userHour, (userTask))
 })
 
 function renderEvent() {
+    // Loop through an eventArr
     for (var i = 0; i < eventArr.length; i++) {
         var hourIndex = "#"
+        // concat # on to eventArr at current index, store result in new variable
         index = hourIndex.concat(eventArr[i])
+        // get eventArr at current index from local storage, store it in new variable
         var tasks = localStorage.getItem(eventArr[i])
+        // set text content of index to tasts
         $(index).text(tasks)
     }
     
 }
 
 function init () {
+    // Loop through an eventArr
     for (var i = 0; i < eventArr.length; i++) {
+        // get eventArr at current index from local storage, store it in new variable
         var savedTasks = (localStorage.getItem(eventArr[i]))
+        // if savedTasks is null/empty, store eventArr at current index position in local storage
             if (savedTasks === null) {
                 localStorage.setItem(eventArr[i], "")
             }
